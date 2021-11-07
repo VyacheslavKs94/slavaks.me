00e009192c4518511703411853fa55d8287c6029
VyacheslavKs94 <62070454+VyacheslavKs94@users.noreply.github.com>
2021-11-08 01:19:50 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -2,8 +2,8 @@ function reqListener () {
     console.log(this.responseText);
 
   }
-  
-  var oReq = new XMLHttpRequest();
-  oReq.onload = reqListener;
-  oReq.open("get", "db.csv", true);
-  oReq.send();
+
+var oReq = new XMLHttpRequest();
+oReq.onload = reqListener;
+oReq.open("get", "db.csv", true);
+oReq.send();
